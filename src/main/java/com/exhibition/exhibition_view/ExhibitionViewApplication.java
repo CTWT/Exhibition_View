b3859abbeef8608b0f523c766a54e0e99b7d0c8e
신인철 <setia@sin-incheol-ui-MacBookAir.local>
2022-07-10 18:18:40 +0900

    전시 일정 및 장소와 리뷰 게시판 만들기
    전시 일정 및 장소와 리뷰 게시판을 제작
    시작 커밋

---
+++ src/main/java/com/exhibition/exhibition_view/ExhibitionViewApplication.java
@@ -0,0 +1,13 @@
+package com.exhibition.exhibition_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExhibitionViewApplication.class, args);
+    }
+
+}
