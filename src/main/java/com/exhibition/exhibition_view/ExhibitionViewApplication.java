5cf1906f0f07941a65968d7bbb51192b235bba3f
setia <inchul9603@gmailc.com>
2022-07-19 01:12:53 +0900

    html 파일 추가
    html 기본적인 서식파일 추가

--- src/main/java/com/exhibition/exhibition_view/ExhibitionViewApplication.java
+++ src/main/java/com/exhibition/exhibition_view/ExhibitionViewApplication.java
@@ -2,7 +2,9 @@ package com.exhibition.exhibition_view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ExhibitionViewApplication {
 
